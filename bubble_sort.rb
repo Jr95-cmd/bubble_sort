1e983aa7838ce9c75083f6408072debebe1d6fd0
Jr95-cmd <94499879+Jr95-cmd@users.noreply.github.com>
2024-02-07 20:36:03 -0400

    reduced code for bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,13 @@
 def bubble_sort (input_arr)
-    input_arr.each do |element|
+    iinput_arr.each do |element|
         x=1
         position=0
         while x < input_arr.length
             if input_arr[position] > input_arr[x] then
                 input_arr.insert(x,input_arr.delete_at(position))
-                position+=1
-                x+=1
-            else
-                x+=1
-                position+=1
             end
+            position+=1
+            x+=1
         end
         puts input_arr
     end
